7fadd6446f909c5e68ba34e10f986d33cbc68f4f
sardap <pfsarda23@gmail.com>
2018-03-27 13:01:39 +1100

    Updated Data Producer and consumer GUI created example UI bundles

---
+++ public-onchain_prototype/prototype/WorkAuthBlockChain/src/DataBundle.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WorkAuthBlockChain.src
+{
+    public class DataBundle
+    {
+		public List<Entry> Referees { get; set; }
+
+		public List<Entry> WorkHistory{ get; set; }
+	}
+}
---
+++ public-onchain_prototype/prototype/WorkAuthBlockChain/src/Entry.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WorkAuthBlockChain.src
+{
+    public class Entry
+    {
+		public string Address { get; set; }
+		public string Domain { get; set; }
+		public string Department { get; set; }
+		public string Position { get; set; }
+		public string Name { get; set; }
+		public string ExtraData { get; set; }
+
+		public string OnChainString()
+		{
+			return Domain + "," + Department + "," + Position + "," + Name + "," + ExtraData;
+		}
+
+		public override string ToString()
+		{
+			return Address + "," + OnChainString();
+		}
+	}
+}
